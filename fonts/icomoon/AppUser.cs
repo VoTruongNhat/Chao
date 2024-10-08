ae97580325a462719a8d32cf9565c3f58fe524b1
VoTruongNhat <vonhut123a@gmail.com>
2024-10-08 23:17:20 +0700

    moi

---
+++ fonts/icomoon/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace BakeryOnline_MVC.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Column("DayOfBirth")]
+        public DateTime? DayOfBirth { get; set; }
+
+        [Column("CreationTime")]
+        public DateTime CreationTime { get; set; }
+    }
+}
---
+++ fonts/icomoon/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BakeryOnline_MVC.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        [Column("Category_ID")]
+        public int ID { get; set; }
+
+        [Required]
+        [Column("Category_Name")]
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
