aa0f77eb61cd3d5b47559744720c870dcc9d55b6
VoTruongNhat <vonhut123a@gmail.com>
2024-10-08 23:19:15 +0700

    Add files via upload

---
+++ fonts/icomoon/Paging.cs
@@ -0,0 +1,10 @@
+﻿namespace BakeryOnline_MVC.Models
+{
+    public class Paging<T>
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPage { get; set; }
+
+        public List<T> Result { get; set; }
+    }
+}
